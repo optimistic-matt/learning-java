5034ac0f16b1e537f4789687f7b840a9a5a21716
optimistic-matt <matheusgomescbrl@gmail.com>
2023-04-03 19:21:56 -0300

    first test file

---
+++ information_test.java
@@ -0,0 +1,21 @@
+package practice;
+
+public class information_test {
+	public static void main(String[] args) {
+		String User = "Matt";
+		int Age = 24;
+		char Gender = 'M';
+		double Height = 1.80;
+		if (User == "Matt" && Age == 24 && Gender == 'M' && Height == 1.80) {
+			System.out.println("Welcome back");
+		}else {
+			System.out.println("Your personal info needs to be updated.");
+		}
+		
+		
+		
+		
+		
+		
+	}
+}
